4bbbbc38df29cc3f6b70126f7d3b52b20a3b34cf
AlexFangSW <alexfangsw@gmail.com>
2025-03-16 16:44:05 +0800

    chore: remove unused paths

--- src/app/multi/test/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import TypingGame from "@/components/TypingContainer";
-export default function Page() {
-  return (
-    <>
-      <TypingGame target={"the quick brown fox jumps over the lazy dog"} />
-    </>
-  );
-}
